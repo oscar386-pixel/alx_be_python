55a9745fafa9b54cfe5041ccb521e5f56cc1b1ab
Oluka Toochukwu <oscartooluka@gmail.com>
2025-11-10 14:47:24 +0100

    Add simple interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Assign values to variables
+
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculate simple interest
+interest = principal * rate * time
+
+# Print the result
+print(F"The simple interest is: {interest}")
